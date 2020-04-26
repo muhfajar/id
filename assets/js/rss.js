f2e213a5e6a41f0c84537dec2dfac182de5e297e
Muhamad Fajar <mfajar0910@gmail.com>
2020-04-27 04:23:49 +0700

    Fix blog list data

--- assets/js/rss.js
+++ assets/js/rss.js
@@ -1,23 +1,46 @@
 const rssUrl = ["https://muhfajar.blog/posts/index.xml", "https://muhfajar.blog/id/posts/index.xml"];
 let parser = new RSSParser();
 let blogList = document.getElementById("blog-list");
+let blogListData = [];
+let blogCount = 0;
+
+function blogListCollector(item, cb) {
+    setTimeout(() => {
+        blogListData.push(item);
+        cb();
+    }, 100);
+}
 
 rssUrl.forEach((url) => {
     parser.parseURL(url, (err, feed) => {
         if (err) throw err;
 
-        const sortedEnFeed = feed.items.sort(function (a, b) {
+        const sortedFeed = feed.items.sort((a, b) => {
+            blogCount++;
             return new Date(b.isoDate) - new Date(a.isoDate);
         });
 
-        sortedEnFeed.forEach((entry) => {
-            const listItem = document.createElement("li");
-            const itemLink = document.createElement("a");
+        let requests = sortedFeed.map((item) => {
+            return new Promise((resolve) => {
+                blogListCollector(item, resolve);
+            });
+        });
 
-            itemLink.textContent = entry.title;
-            itemLink.setAttribute('href', entry.link);
-            listItem.appendChild(itemLink);
-            blogList.appendChild(listItem);
+        Promise.all(requests).then(() => {
+            if (blogListData.length === blogCount) {
+                blogListData.sort((a, b) => {
+                    return new Date(b.isoDate) - new Date(a.isoDate)
+                }).forEach((entry) => {
+                    const listItem = document.createElement("li");
+                    const itemLink = document.createElement("a");
+
+                    itemLink.textContent = entry.title;
+                    itemLink.setAttribute("href", entry.link);
+                    itemLink.setAttribute("title", entry.pubDate);
+                    listItem.appendChild(itemLink);
+                    blogList.appendChild(listItem);
+                });
+            }
         });
     });
 });
